0762eaa0a02261ee4c98eb952d2f8ccac16f1d0a
Will Fitzgerald <willf@github.com>
2016-10-04 15:15:31 -0400

    add AZO

--- multiclock.js
+++ multiclock.js
@@ -6,6 +6,7 @@ setInterval(function(){
   $("#columbus_time").text(local.clone().tz("America/New_York").format("HH:mm"));
   $("#huntsville_time").text(local.clone().tz("America/Chicago").format("HH:mm"));
   $("#sf_time").text(local.clone().tz("America/Los_Angeles").format("HH:mm"));
+  $("#azo_time").text(local.clone().tz("America/New_York").format("HH:mm"));
   $("#utc_time").text(local.clone().utc().format("HH:mm"));
   $("#epoch_time").text(local.unix());
 },1000);
