71b786e2ed81426648a776bd13e17b7f7da3722c
Will Fitzgerald <willf@github.com>
2016-10-18 13:11:42 -0400

    Beautify javascript

--- multiclock.js
+++ multiclock.js
@@ -1,12 +1,11 @@
-
-setInterval(function(){
-  var local    = moment();
-  $("#local_date").text(local.format("MMMM D, YYYY"));
-  $("#local_time").text(local.format("HH:mm:ss"));
-  $("#columbus_time").text(local.clone().tz("America/New_York").format("HH:mm"));
-  $("#huntsville_time").text(local.clone().tz("America/Chicago").format("HH:mm"));
-  $("#sf_time").text(local.clone().tz("America/Los_Angeles").format("HH:mm"));
-  $("#azo_time").text(local.clone().tz("America/New_York").format("HH:mm"));
-  $("#utc_time").text(local.clone().utc().format("HH:mm"));
-  $("#epoch_time").text(local.unix());
-},1000);
+setInterval(function() {
+    var local = moment();
+    $("#local_date").text(local.format("MMMM D, YYYY"));
+    $("#local_time").text(local.format("HH:mm:ss"));
+    $("#columbus_time").text(local.clone().tz("America/New_York").format("HH:mm"));
+    $("#huntsville_time").text(local.clone().tz("America/Chicago").format("HH:mm"));
+    $("#sf_time").text(local.clone().tz("America/Los_Angeles").format("HH:mm"));
+    $("#azo_time").text(local.clone().tz("America/New_York").format("HH:mm"));
+    $("#utc_time").text(local.clone().utc().format("HH:mm"));
+    $("#epoch_time").text(local.unix());
+}, 1000);
