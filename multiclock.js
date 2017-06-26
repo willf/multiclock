df15a77103d1142e785f1b2d8307a325c21a1248
Will Fitzgerald <willf@github.com>
2017-06-26 15:13:25 -0400

    add japan

--- multiclock.js
+++ multiclock.js
@@ -2,6 +2,7 @@ setInterval(function() {
     var local = moment();
     $("#local_date").text(local.format("MMMM D, YYYY"));
     $("#local_time").text(local.format("HH:mm:ss"));
+    $("#japan_time").text(local.clone().tz("Asia/Tokyo").format("HH:mm"));
     $("#columbus_time").text(local.clone().tz("America/New_York").format("HH:mm"));
     $("#huntsville_time").text(local.clone().tz("America/Chicago").format("HH:mm"));
     $("#sf_time").text(local.clone().tz("America/Los_Angeles").format("HH:mm"));
