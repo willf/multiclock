2f9651f6cb9a6d483c502cb55518984a8463cd05
Will Fitzgerald <willf@github.com>
2020-09-11 15:06:41 -0400

    Update to time zones

--- multiclock.js
+++ multiclock.js
@@ -1,12 +1,12 @@
 setInterval(function() {
     var local = moment();
-    $("#local_date").text(local.format("MMMM D, YYYY"));
+    $("#local_date").text(local.format("YY-MM-DD"));
     $("#local_time").text(local.format("HH:mm:ss"));
     $("#japan_time").text(local.clone().tz("Asia/Tokyo").format("HH:mm"));
-    $("#columbus_time").text(local.clone().tz("America/New_York").format("HH:mm"));
-    $("#huntsville_time").text(local.clone().tz("America/Chicago").format("HH:mm"));
-    $("#sf_time").text(local.clone().tz("America/Los_Angeles").format("HH:mm"));
-    $("#azo_time").text(local.clone().tz("America/New_York").format("HH:mm"));
+    $("#eastern_time").text(local.clone().tz("America/New_York").format("HH:mm"));
+    $("#central_time").text(local.clone().tz("America/Chicago").format("HH:mm"));
+    $("#pacific_time").text(local.clone().tz("America/Los_Angeles").format("HH:mm"));
+    $("#china_time").text(local.clone().tz("Asia/Shanghai").format("HH:mm"));
     $("#utc_time").text(local.clone().utc().format("HH:mm"));
     $("#epoch_time").text(local.unix());
 }, 1000);
