ce503304207a6e4ab5ad5f8aa6f1cc6e860d50d8
Will Fitzgerald <willf@github.com>
2022-05-26 10:04:10 -0400

    update clocks to current team

--- multiclock.js
+++ multiclock.js
@@ -5,9 +5,10 @@ setInterval(function() {
     $("#japan_time").text(local.clone().tz("Asia/Tokyo").format("HH:mm"));
     $("#eastern_time").text(local.clone().tz("America/New_York").format("HH:mm"));
     $("#central_time").text(local.clone().tz("America/Chicago").format("HH:mm"));
+    $("#mountain_time").text(local.clone().tz("America/Denver").format("HH:mm"));
     $("#pacific_time").text(local.clone().tz("America/Los_Angeles").format("HH:mm"));
     $("#china_time").text(local.clone().tz("Asia/Shanghai").format("HH:mm"));
     $("#utc_time").text(local.clone().utc().format("HH:mm"));
-    $("#spain_time").text(local.clone().tz("Europe/Madrid").format("HH:mm"));
+    $("#central_europe_time").text(local.clone().tz("Europe/Madrid").format("HH:mm"));
     $("#epoch_time").text(local.unix());
 }, 1000);
